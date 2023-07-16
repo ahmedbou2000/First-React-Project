a7a73e02aaaf26fe903cad722260225c30fe6feb
test <test>
2023-07-16 22:15:31 +0200

    Realisation de l'exercice du btn qui ajoute pour chaque click un objet ( carte ) dans la page en utilisant this.state

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,13 +1,46 @@
 import React, { Component } from "react";
 import Card from './Card'
 
-class CardList extends Component{
-    render(){
+class CardList extends Component {
+
+    constructor() {
+        super()
+
+        this.state = { nbrObject: 0 }
+        this.clickHandler = this.clickHandler.bind(this)
+    }
+
+    clickHandler() {
+        console.log(this.state.nbrObject)
+        // incrementation de la vateur du nombre des object à afficher :
+        this.setState(prevState => {
+            return { nbrObject: prevState.nbrObject + 1 }
+        })
+
+        // refrech the page : 
+        this.mapping()
+    }
+
+    mapping() {
+        return (
+            this.props.Data.slice(0, this.state.nbrObject).map((cardData) => (
+                <Card cardData={cardData} key={cardData.id} />
+            ))
+        )
+    }
+
+
+
+    render() {
 
         return (
-            this.props.Data.map((cardData) => (
-                <Card  cardData={cardData} />
-              ))
+            <>
+                
+                <button className="btn btn-primary btn-lg btn-block btnAfficher" onClick={this.clickHandler}>Click me !</button>
+
+                {/* afficher les objects */}
+                {this.mapping()}
+            </>
         );
     }
 }
---
+++ src/components/cardListObjects.js
@@ -0,0 +1,49 @@
+const cardList = [
+    {
+      id: 1,
+      image: "https://picsum.photos/200/300",
+      name: "John Doe",
+      function: "Software Engineer",
+      phone: "+1 123-456-7890",
+      email: "john.doe@example.com",
+      website: "https://example.com"
+    },
+    {
+      id: 2,
+      image: "https://picsum.photos/200/301",
+      name: "Jane Smith",
+      function: "Graphic Designer",
+      phone: "+1 987-654-3210",
+      email: "jane.smith@example.com",
+      website: "https://example.com"
+    },
+    {
+      id: 3,
+      image: "https://picsum.photos/200/302",
+      name: "Michael Johnson",
+      function: "Project Manager",
+      phone: "+1 555-555-5555",
+      email: "michael.johnson@example.com",
+      website: "https://example.com"
+    },
+    {
+      id: 4,
+      image: "https://picsum.photos/200/303",
+      name: "Emily Davis",
+      function: "Marketing Specialist",
+      phone: "+1 111-222-3333",
+      email: "emily.davis@example.com",
+      website: "https://example.com"
+    },
+    {
+      id: 5,
+      image: "https://picsum.photos/200/304",
+      name: "Alex Wilson",
+      function: "UX Designer",
+      phone: "+1 999-888-7777",
+      email: "alex.wilson@example.com",
+      website: "https://example.com"
+    }
+  ];
+
+export default cardList;
